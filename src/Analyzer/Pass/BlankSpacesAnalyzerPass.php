abaf5a02ec95f237c06dc9bbd346a033fe5bdfd9
Alexandre Daubois <alex.daubois@gmail.com>
2021-12-21 22:08:13 +0100

    fix: add missing license comments

--- src/Analyzer/Pass/BlankSpacesAnalyzerPass.php
+++ src/Analyzer/Pass/BlankSpacesAnalyzerPass.php
@@ -1,10 +1,22 @@
 <?php
 
+/*
+ * This file is part of IfYouSaySo.
+ *
+ * (c) Alexandre Daubois <alex.daubois@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Analyzer\Pass;
 
 use State\RunnerState;
 use Symfony\Component\Finder\SplFileInfo;
 
+/**
+ * @author Alexandre Daubois <alex.daubois@gmail.com>
+ */
 class BlankSpacesAnalyzerPass implements AnalyzerPassInterface
 {
     public function pass(SplFileInfo $file): void
